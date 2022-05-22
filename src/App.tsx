f4eed3abf5739151014883695b3a8f569d8b155f
Zain Khan <zainkhanjune@gmail.com>
2022-05-22 11:31:51 +0530

    updated the appearance of login form

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ import AllProducts from './pages/AllProducts';
 import Categories from './pages/Categories';
 import IndividualProduct from './pages/IndividualProduct';
 import SignupForm from "./components/SignupForm"
-import Login from "./components/Login"
 import Orders from "./pages/Orders"
 import Cart from "./pages/Cart"
 import Navbar from './components/Navbar';
+import LoginForm from './components/LoginForm';
 
 
 
@@ -28,10 +28,9 @@ const App:React.FC = () => {
             <Route path="/allproducts" element={<AllProducts />} />
             <Route path="/categories" element={<Categories />} />
             <Route path="/register" element={<SignupForm />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<LoginForm />} />
             <Route path="/individual/:id" element={<IndividualProduct />} />
             <Route path="/orders" element={<Orders />} />
-            <Route path="/login" element={<Login />} />
             <Route path="register" element={<SignupForm />} />
             <Route path="/cart" element={<Cart />} />
           </Routes>
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,40 @@
+import { Formik, Form, Field } from "formik"
+
 const LoginForm = () => {
     return(
-        <div>Login is here</div>
+        <div className="md:w-2/5 mx-auto mt-4 p-4"  >
+            <Formik
+                initialValues={{
+                    "email":'',
+                    "password":''
+                }}
+                onSubmit={(values)=>{
+                    console.log(values)
+                }}
+            >
+                <Form className="rounded-xl bg-white  flex flex-col items-center ">
+                <h1 className="my-2 text-4xl p-2 divide-x">Login</h1>
+                <div className="border-2 border-blue-700 w-full" />
+                    <label htmlFor="email" className="font-xl font-bold mt-4">Email</label>
+                    <Field
+                        className="shadow-xl rounded-xl px-3 py-2 my-4"
+                        id="email"
+                        name="email"
+                        type="email" 
+                        placeholder="Enter Email Address"
+                        />
+                    <label htmlFor="password" className="font-xl font-bold mt-2">Password</label>
+                    <Field
+                        className="shadow-xl rounded-xl px-3 py-2 my-4" 
+                        id="password"
+                        name="password"
+                        type="password" 
+                        placeholder="Enter password"
+                        />
+                    <button className="mt-5 mb-4 rounded-xl px-4 py-2 shadow-xl bg-blue-700  text-white text-xl" type="submit">Login</button>
+                </Form>
+            </Formik>
+        </div>
     )
 }
 export default LoginForm
