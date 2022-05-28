ef4b1754f374edb96f79985206c1ed7ead44fb12
Zain Khan <zainkhanjune@gmail.com>
2022-05-28 15:50:16 +0530

    Added table for specs

--- src/components/ProductSpecs.tsx
+++ src/components/ProductSpecs.tsx
@@ -1,7 +1,37 @@
 const ProductSpecs = () => {
     return (
-        <div className="text-center">
-            SPecs here
+        <div className="bg-white text-center flex flex-col items-center rounded-xl my-4">
+            <div className="text-2xl font-bold my-2">Specifications</div>
+            <table className="table-fixed w-9/12 mx-4 mt-4 border-1 border-black">
+                <thead>
+                    <th className="text-xl border border-slate-300 bg-gray-600 text-white">Name</th>
+                    <th className="text-xl border border-slate-300 bg-gray-600 text-white" >Value</th>
+                </thead>
+                <tr>
+                    <td className="bg-gray-300 p-2 text-lg">Width</td>
+                    <td className="bg-gray-300 p-2 tex-lg">30</td>
+                </tr>
+                <tr>
+                    <td className="bg-gray-300 p-2 text-lg">Width</td>
+                    <td className="bg-gray-300 p-2 tex-lg">30</td>
+                </tr>
+                <tr>
+                    <td className="bg-gray-300 p-2 text-lg">Width</td>
+                    <td className="bg-gray-300 p-2 tex-lg">30</td>
+                </tr>
+                <tr>
+                    <td className="bg-gray-300 p-2 text-lg">Width</td>
+                    <td className="bg-gray-300 p-2 tex-lg">30</td>
+                </tr>
+                <tr>
+                    <td className="bg-gray-300 p-2 text-lg">Width</td>
+                    <td className="bg-gray-300 p-2 tex-lg">30</td>
+                </tr>
+                <tr>
+                    <td className="bg-gray-300 p-2 text-lg">Width</td>
+                    <td className="bg-gray-300 p-2 tex-lg">30</td>
+                </tr>
+            </table>
         </div>
     )
 }
