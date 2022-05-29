6dbbe8b9e2418692165dc5109e923472d9a5e4cb
Zain Khan <zainkhanjune@gmail.com>
2022-05-29 13:06:56 +0530

    Added search bar ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { setLogged } from "../reducer/LoginSlice"
 import {useState} from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "../reducer/store"
-
-
+import {BsSearch} from "react-icons/bs"
+import {useNavigate} from "react-router-dom"
 
 
 const Navbar = () => {
@@ -19,6 +19,7 @@ const Navbar = () => {
         dispatch(setLogged(false))
     }
     
+    const navigate = useNavigate()
 
     const isLogged:boolean = useSelector((state:RootState)=>state.login.isLogged)
 
@@ -30,8 +31,15 @@ const Navbar = () => {
                <Link to="/home" ><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">Home</div></Link>
                <Link to="/category"><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">Categories</div></Link>
                <Link to="/products" ><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">All Product</div></Link>
-               <Link to="/login"><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">Login</div></Link>
-               <Link to="/register"><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">Register</div></Link>
+               <input className="rounded-l-xl  ml-4 flex-1 focus:outline-none p-2 " />
+               <button className="px-3 py-2 bg-blue-600 mr-2"><BsSearch className=" text-white"  /></button>
+               <select className="bg-blue-200 focus:outline-none hover:cursor-pointer">
+                   <option hidden selected>Login/Signup</option>
+                   <option onClick={()=> navigate('/login')} className=" bg-white text-xl">Login</option>
+                   <option onClick={()=> navigate('/register')} className="bg-white text-xl ">Signup</option>
+               </select>
+               {/* <Link to="/login"><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">Login</div></Link>
+               <Link to="/register"><div className="hover:bg-blue-600 hover:text-white p-2 hover:rounded-xl">Register</div></Link> */}
                 {/* <button  onClick={handleDialogAction}> Signup</button> */}
             </div>
         )
