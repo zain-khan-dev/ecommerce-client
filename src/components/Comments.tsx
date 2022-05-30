0e0a4c075701f3369e2a53f619fa7b8fffd676bb
Zain Khan <zainkhanjune@gmail.com>
2022-05-30 08:33:33 +0530

    Added ui for writing review

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,20 +1,36 @@
 import {CommentSchema} from "../utillity/Constants"
+import { useAuthenticationStatus } from "../utillity/utils"
 import SingleCommentPanel from "./SingleCommentPanel"
+import {useState} from "react"
+import Modal from "./Modal"
 
 interface Props {
     comments: CommentSchema[]
 }
 
+
+
+
 const Comments:React.FC<Props> = ({comments}) => {
+
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const handleWriteReview = () => {
+        console.log("Write review")
+        setIsOpen(true)
+    }
+
+    const status = useAuthenticationStatus("/comments")
+
     return (
         <div className="mt-2 bg-white flex flex-col items-center rounded-xl">
             <h1 className="mt-2 text-2xl font-bold">Comments & Reviews</h1>
+            {status?<button onClick={handleWriteReview} className="bg-yellow-600 text-white p-2 rounded-xl my-2">Write Review</button>:<div></div>}
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
             {comments.map((comment)=>(
                 <SingleCommentPanel comment={comment} />
             ))}
-                <SingleCommentPanel comment={comments[0]} />
-                <SingleCommentPanel comment={comments[0]} />
-                <SingleCommentPanel comment={comments[0]} />
         </div>
     )    
 }
---
+++ src/components/Modal.tsx
@@ -0,0 +1,83 @@
+import {Dialog, Transition} from "@headlessui/react"
+import {useState, Fragment} from "react"
+
+interface Props {
+    isOpen:boolean;
+    setIsOpen:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+
+const Modal:React.FC<Props> = ({isOpen, setIsOpen}) => {
+
+
+
+    const [title, setTitle] = useState<string>("")
+    const [description, setDescription] = useState<string>("")
+
+    const [rating, setRating] = useState<number>(0)
+
+    const submitReview = () => {
+
+        console.log(title, description, rating)
+
+    }
+
+    return (
+        <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={()=>setIsOpen(false)}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h1"
+                    className="text-2xl font-bold text-center leading-6 text-gray-900"
+                  >
+                      Write Review
+                  </Dialog.Title>
+                  <div className="bg-gray-200 p-2 mt-4 flex flex-col justify-center items-center">
+                        <label className="block text-lg p-2 font-semibold">Enter Rating</label>
+                        <select value={rating} onChange={(e)=>setRating(parseInt(e.target.value))} className="bg-white p-2 mx-auto">
+                            {[...Array(5)].map((val, idx)=>(
+                                <option value={idx+1}>{idx+1}</option>
+                            ))}
+                        </select>
+                        <label className="block text-lg p-2 font-semibold">Enter Title</label>
+                        <input value={title} onChange={(e)=>setTitle(e.target.value)} className="shadow-xl p-1" /><br/>
+                        <label className="block text-lg p-2 font-semibold">Enter Description</label>
+                        <textarea value={description} onChange={(e)=>setDescription(e.target.value)} cols={30} className="shadow-xl p-1" /><br />
+                        <div className="mt-4 ">
+                            <button onClick={submitReview} className="bg-blue-600 text-white px-3 py-2 mt-2 rounded-xl">Submit</button>
+                            <button onClick={()=>setIsOpen(false)} className="bg-yellow-600 text-white px-3 py-2 mt-2 rounded-xl ml-2">Cancel</button>
+                        </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    )
+}
+
+export default Modal
